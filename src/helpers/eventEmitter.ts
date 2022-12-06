cd5dcaabe4a26f977ee102882b57e48dcf85aeb9
Tural <tmukhtarly@mail.ru>
2022-12-06 21:42:08 +0300

    delete files

--- src/helpers/eventEmitter.ts
+++
@@ -1,8 +0,0 @@
-import Phaser from 'phaser';
-
-export const WIN_EVENT = 'congratulations';
-export const LOSE_EVENT = 'game_over';
-
-const eventEmitter = new Phaser.Events.EventEmitter();
-
-export default eventEmitter;
--- src/helpers/textGenerate.ts
+++
@@ -1,6 +0,0 @@
-import {CONGRATULATIONS_TEXT, GAME_OVER_TEXT, LOSE_TEXT, WIN_TEXT} from "../constants/textsConstants";
-
-export const generateText = (text: string): any => {
-    if (text === WIN_TEXT) return alert(CONGRATULATIONS_TEXT);
-    if (text === LOSE_TEXT) return alert(GAME_OVER_TEXT);
-}
